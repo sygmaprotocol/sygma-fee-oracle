ffef3686345efc53e32650750a0248fcd5e35262
Nikola <nikola.mlinaric93@gmail.com>
2022-09-19 11:28:38 +0200

    Fix debug mode values

--- api/debugModeApi.go
+++ api/debugModeApi.go
@@ -4,12 +4,13 @@
 package api
 
 import (
-	"github.com/ChainSafe/sygma-fee-oracle/config"
-	"github.com/gin-gonic/gin"
-	"github.com/pkg/errors"
 	"net/http"
 	"strconv"
 	"time"
+
+	"github.com/ChainSafe/sygma-fee-oracle/config"
+	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
 )
 
 func (h *Handler) debugGetRate(c *gin.Context) {
@@ -33,16 +34,17 @@ func (h *Handler) debugGetRate(c *gin.Context) {
 	}
 	endpointRespData := &FetchRateResp{
 		BaseRate:                 "0.000445",
-		TokenRate:                "8.948864",
-		DestinationChainGasPrice: "9000000000",
+		TokenRate:                "15.948864",
+		DestinationChainGasPrice: "2000000000",
 		FromDomainID:             fromDomainID,
 		ToDomainID:               toDomainID,
 		DataTimestamp:            time.Now().Unix(),
 		SignatureTimestamp:       time.Now().Unix(),
-		ExpirationTimestamp:      h.dataExpirationManager(time.Now().Unix()),
+		ExpirationTimestamp:      h.dataExpirationManager(time.Now().Unix() + 100000000),
 		Debug:                    true,
 		ResourceID:               resourceID,
 	}
+
 	endpointRespData.Signature, err = h.rateSignature(endpointRespData, fromDomainID, resourceID)
 	if err != nil {
 		ginErrorReturn(c, http.StatusInternalServerError, newReturnErrorResp(&config.ErrIdentityStampFail, err))
