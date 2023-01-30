eeae1ccd473cd6be5710366b89216c13744a5cb0
Matija Petrunic <matija.petrunic@gmail.com>
2023-01-30 18:01:11 +0100

    Fix start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,6 +6,6 @@
 dir=${PWD}
 
 # only run this script when doing end to end testing
-sygma-fee-oracle server -c $dir/e2e/config/config.yaml -d $dir/domain.json -r $dir/resource.json -k $dir/e2e/keystore/test_keyfile.priv -t secp256k1 >> $dir/feeOracle.log 2>&1 &
+sygma-fee-oracle server -c $dir/e2e/config/config.yaml -d $dir/domain.json -k $dir/e2e/keystore/test_keyfile.priv -t secp256k1 >> $dir/feeOracle.log 2>&1 &
 
 sleep 5
